28a0a3d4fea00b3f8947ec0ac3aff59172bc7e44
kundan77 <kundan.pinnaiah01@gmail.com>
2026-06-09 23:06:48 +0530

    Sample Project change

---
+++ src/main/java/com/knd/demo/EmployeeModel.java
@@ -0,0 +1,32 @@
+package com.knd.demo;
+
+public class EmployeeModel {
+
+    private String empNo;
+    private String empName;
+    private String designation;
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+}
